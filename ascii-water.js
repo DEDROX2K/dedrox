337fbd368a7956944323a16f29f3b608d5fcd47c
raghavaircards <raghavprasanna2000@gmail.com>
2026-04-18 13:06:58 +0100

    A

--- ascii-water.js
+++ ascii-water.js
@@ -11,14 +11,14 @@
 
     // --- 🎛️ SETTINGS ---
     const SETTINGS = {
-        gridSize: 20,       // Smaller cells for more detail
-        viscosity: 0.98,    // How much velocity is kept (0.9-0.99)
-        diffusion: 0.97,    // How much 'residue' is kept (0.9-0.99)
+        gridSize: 30,       // Smaller cells for more detail
+        viscosity: 0.93,    // How much velocity is kept (0.9-0.99)
+        diffusion: 0.98,    // How much 'residue' is kept (0.9-0.99)
         mouseForce: 0.4,    // Pushing strength
-        residueStrength: 29, // Added density per move
-        fontSize: 14,
-        blue: '#a1a1a1ff',    // AirPaste Blue
-        white: '#ff0000ff',
+        residueStrength: 89, // Added density per move
+        fontSize: 28,
+        blue: '#4E706A',    // AirPaste Blue
+        white: '#ffffffff',
         coverImgSrc: 'images/bg19.jpg'
     };
 
